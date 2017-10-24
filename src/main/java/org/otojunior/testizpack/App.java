ff371b4f5db460b2827598229e29b520eba79fee
Oto Soares Coelho Junior <oto.coelho-junior@serpro.gov.br>
2017-10-24 17:13:42 -0200

    Initial Commit

---
+++ src/main/java/org/otojunior/testizpack/App.java
@@ -0,0 +1,17 @@
+package org.otojunior.testizpack;
+
+/**
+ * Application Main Class.
+ *
+ * @author Oto Junior
+ * @version $Id: $Id
+ */
+public class App {
+	/**
+	 * Application main method.
+	 * @param args Command line arguments.
+	 */
+	public static void main(String[] args) {
+		new MainWindow().setVisible(true);
+	}
+}
---
+++ src/main/java/org/otojunior/testizpack/MainWindow.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package org.otojunior.testizpack;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+/**
+ * Janela Principal
+ *
+ * @author Oto Junior
+ * @version $Id: $Id
+ */
+public class MainWindow extends JFrame {
+	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * Construtor padrão.
+	 */
+	public MainWindow() {
+		this.setSize(500, 200);
+		this.setLocation(500,  500);
+		this.setResizable(false);
+		this.setTitle("Main Window - Test");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JButton btnCancel = new JButton("Cancel");
+		btnCancel.addActionListener(e -> {
+			MainWindow.this.setVisible(false);
+			MainWindow.this.dispose();
+			System.exit(0);
+		});
+		
+		JPanel painelBotoes = new JPanel();
+		painelBotoes.add(btnCancel);
+		
+		String str = 
+				"java.version: " + System.getProperty("java.version") + "\n" +
+				"java.home: " + System.getProperty("java.home") + "\n" +
+				"java.vm.name: " + System.getProperty("java.vm.name") + "\n" +
+				"java.class.version: " + System.getProperty("java.class.version") + "\n" +
+				"user.dir: " + System.getProperty("user.dir");
+			
+		JTextArea textArea = new JTextArea(str);
+		textArea.setEditable(false);
+		textArea.setAutoscrolls(false);
+		
+		this.add(painelBotoes, BorderLayout.SOUTH);
+		this.add(textArea, BorderLayout.CENTER);
+	}
+}
